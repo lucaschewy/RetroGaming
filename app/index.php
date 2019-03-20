7eafd2ef8e78e67a019d6f57d357978b6027a24a
“Alexis <alexisgerbaud@gmail.com>
2019-03-20 22:35:54 +0100

    design

--- app/index.php
+++ app/index.php
@@ -196,7 +196,7 @@
             </div>
           </div>
           <div class="carousel-item">
-            <img src="src/img/mario_dr.jpg" class="d-block w-100" alt="...">
+            <img src="src/img/magie.jpg" class="d-block w-100" alt="...">
               <div class="content-slide">
                 <div class="img-6"></div>
                 <div class="txt">
@@ -260,7 +260,7 @@
               </div>
             </div>
           <div class="carousel-item">
-            <img src="src/img/sonic_dr.jpg" class="d-block w-100" alt="...">
+            <img src="src/img/chat.jpg" class="d-block w-100" alt="...">
               <div class="content-slide">
                 <div class="img-10"></div>
                 <div class="txt">
