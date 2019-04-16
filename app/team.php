dd08de32ffa2caa61a1b43d7516525a4c5af7d06
fleurissonalexis <fleurisson.alexis@hotmail.fr>
2019-04-16 10:01:23 +0200

    test

--- app/team.php
+++ app/team.php
@@ -130,7 +130,7 @@
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">Charlélie Bessonnet</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">test</p>
                     <a href="https://www.linkedin.com/in/charlelie-bessonnet/" class="btn btn-primary"><i class="fab fa-linkedin"></i> Profil</a>
                 </div>
             </div>
